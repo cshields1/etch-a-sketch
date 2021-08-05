1ae61b1253d4a8bfd48a49c6f9d26654c7594d16
Craig <84283410+cshields1@users.noreply.github.com>
2021-08-05 13:32:58 -0700

    Implement base functionality and box sizing

--- scripts.js
+++ scripts.js
@@ -1,40 +1,44 @@
-const gridContainer = document.querySelector("#grid-container");
 const resetButton = document.querySelector("button");
+const gridContainer = document.querySelector("#grid-container");
 
 createSquares();
 
-const squares = document.querySelectorAll(".square");
-
-for (let square of squares) {
-  square.addEventListener("mouseenter", () => square.classList.add("hover"));
-  //   square.addEventListener(
-  //     "mouseleave",
-  //     () => (square.style.backgroundColor = "blue")
-  //   );
-}
-
 resetButton.addEventListener("click", () => {
-  for (let square of squares) {
-    square.classList.remove("hover");
+  clearGrid();
+  let gridSet = prompt("How many squares per side? (1-100)");
+  if (gridSet > 100) {
+    while (gridSet > 100) {
+      gridSet = prompt("How many squares per side? (1-100)");
+    }
   }
-  let gridSet = prompt("How many squares? (S x S)");
+  if (!gridSet) gridSet = 16;
   createSquares(gridSet);
 });
 
 function createSquares(gridSet = 16) {
   for (let i = 1; i <= gridSet; i++) {
     let row = document.createElement("div");
-    row.classList.add(
-      "row",
-      i,
-      `row-cols-${gridSet}`,
-      "justify-content-center"
-    );
+    row.classList.add("row", i);
+
     for (let i = 1; i <= gridSet; i++) {
       let square = document.createElement("div");
-      square.classList.add("square", i, "col");
+      square.classList.add("square", i);
       row.appendChild(square);
     }
     gridContainer.appendChild(row);
+
+    const squares = document.querySelectorAll(".square");
+
+    for (let square of squares) {
+      square.addEventListener("mouseenter", () =>
+        square.classList.add("hover")
+      );
+    }
+  }
+}
+
+function clearGrid() {
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
   }
 }
