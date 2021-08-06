ee4f2c2903e359e9cd7ea3567d8c273cd6d29c33
Craig <84283410+cshields1@users.noreply.github.com>
2021-08-06 10:28:38 -0700

    Refactor grid logic

--- scripts.js
+++ scripts.js
@@ -3,15 +3,7 @@ const gridContainer = document.querySelector("#grid-container");
 
 createSquares();
 
-resetButton.addEventListener("click", () => {
-  clearGrid();
-  let gridSet = prompt("How many squares per side? (1-100)");
-  while (gridSet > 100) {
-    gridSet = prompt("How many squares per side? (1-100)");
-  }
-  if (!gridSet) gridSet = 16;
-  createSquares(gridSet);
-});
+resetButton.addEventListener("click", () => resetGrid());
 
 function createSquares(gridSet = 16) {
   for (let i = 1; i <= gridSet; i++) {
@@ -21,18 +13,13 @@ function createSquares(gridSet = 16) {
     for (let i = 1; i <= gridSet; i++) {
       let newSquare = document.createElement("div");
       newSquare.classList.add("square", i);
+      newSquare.addEventListener(
+        "mouseenter",
+        () => (newSquare.style.backgroundColor = makeRandomColor())
+      );
       newRow.appendChild(newSquare);
     }
     gridContainer.appendChild(newRow);
-
-    const allSquares = document.querySelectorAll(".square");
-
-    for (let square of allSquares) {
-      square.addEventListener(
-        "mouseenter",
-        () => (square.style.backgroundColor = makeRandomColor())
-      );
-    }
   }
 }
 
@@ -41,8 +28,14 @@ function makeRandomColor() {
   return `rgb(${randomRGBValue()},${randomRGBValue()},${randomRGBValue()})`;
 }
 
-function clearGrid() {
+function resetGrid() {
   while (gridContainer.firstChild) {
     gridContainer.removeChild(gridContainer.firstChild);
   }
+  let gridSet = prompt("How many squares per side? (1-100)");
+  while (gridSet > 100) {
+    gridSet = prompt("How many squares per side? (1-100)");
+  }
+  if (!gridSet) gridSet = 16;
+  createSquares(gridSet);
 }
