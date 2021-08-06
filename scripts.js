989146579a8e30771b6fe52b918027bbdf01703a
Craig <84283410+cshields1@users.noreply.github.com>
2021-08-06 12:47:15 -0700

    Add gradient functionality

--- scripts.js
+++ scripts.js
@@ -1,9 +1,15 @@
-const resetButton = document.querySelector("button");
+const resetButton = document.querySelector("#reset-btn");
 const gridContainer = document.querySelector("#grid-container");
+const grayscaleButton = document.querySelector("#grayscale-btn");
+const rainbowButton = document.querySelector("#rainbow-btn");
+const grainbowButton = document.querySelector("#grainbow-btn");
 
 createSquares();
 
 resetButton.addEventListener("click", () => resetGrid());
+grayscaleButton.addEventListener("click", () => setGrayscaleMode());
+rainbowButton.addEventListener("click", () => setRainbowMode());
+grainbowButton.addEventListener("click", () => setGrainbowMode());
 
 function createSquares(gridSet = 16) {
   for (let i = 1; i <= gridSet; i++) {
@@ -11,16 +17,17 @@ function createSquares(gridSet = 16) {
     newRow.classList.add("row", i);
 
     for (let i = 1; i <= gridSet; i++) {
-      let newSquare = document.createElement("div");
-      newSquare.classList.add("square", i);
-      newSquare.addEventListener(
-        "mouseenter",
-        () => (newSquare.style.backgroundColor = makeRandomColor())
-      );
-      newRow.appendChild(newSquare);
+      let square = document.createElement("div");
+      square.classList.add("square", i);
+      square.addEventListener("mouseover", setRainbow);
+      newRow.appendChild(square);
     }
     gridContainer.appendChild(newRow);
   }
+}
+
+function setRainbow() {
+  this.style.background = makeRandomColor();
 }
 
 function makeRandomColor() {
@@ -38,4 +45,43 @@ function resetGrid() {
   }
   if (!gridSet) gridSet = 16;
   createSquares(gridSet);
+}
+
+function setGrayscaleMode() {
+  const squares = document.querySelectorAll(".square");
+  for (let square of squares) {
+    square.removeEventListener("mouseover", setRainbow);
+    square.pass = 0;
+    square.addEventListener("mouseover", setGrayscale);
+  }
+}
+
+function setGrayscale() {
+  this.style.backgroundColor = "black";
+  this.pass += 1;
+  if (this.style.opacity < 1) this.style.opacity = 0.1 * this.pass;
+}
+
+function setRainbowMode() {
+  const squares = document.querySelectorAll(".square");
+  for (let square of squares) {
+    square.removeEventListener("mouseover", setGrayscale);
+    square.addEventListener("mouseover", setRainbow);
+  }
+}
+
+function setGrainbowMode() {
+  const squares = document.querySelectorAll(".square");
+  for (let square of squares) {
+    square.removeEventListener("mouseover", setRainbow);
+    square.removeEventListener("mouseover", setGrayscale);
+    square.pass = 0;
+    square.addEventListener("mouseover", setGrainbow);
+  }
+}
+
+function setGrainbow() {
+  this.style.background = makeRandomColor();
+  this.pass += 1;
+  if (this.style.opacity < 1) this.style.opacity = 0.2 * this.pass;
 }
